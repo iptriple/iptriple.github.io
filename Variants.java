0b7f1c9a0ab94549622ce01d6194c7a261434a72
iptriple <andrii9422@gmail.com>
2020-12-08 22:48:12 +0200

    комітнись ужеее

--- Variants.java
+++ Variants.java
@@ -2,6 +2,6 @@ import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        System.out.print("Hello github!!");
+        System.out.print("Hello github!!!");
     }
 }    
